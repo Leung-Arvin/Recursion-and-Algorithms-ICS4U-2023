cbf987d94416af8c3024f4419f7a9ed397288ec4
ArvinLeung1 <9591496-ArvinLeung1@users.noreply.replit.com>
2023-05-02 00:20:34 +0000

    stuff happened

--- Insertion.java
+++ Insertion.java
@@ -5,9 +5,10 @@ public class Insertion extends SortingAlgorithm {
   }
 
   public static void sort() {
+   
     System.out.println(array.length);
     for (int i = 1; i < array.length; i++) {
-   
+      System.out.printf("----Inserting element %d----%n", i);
       Insert(i);
      
       
@@ -23,29 +24,48 @@ public class Insertion extends SortingAlgorithm {
       return;
     }
 
-    System.out.printf("----Inserting element %d----%n", index+1);
+   
     
     int current, previous;
   
     current = array[index];
     previous = array[index - 1];
 
-  
+    
+    
     while (index > 0 && array[index - 1] > array[index]) {
-
+      
       //current = array[index];
       //previous = array[index - 1];
       
       //swap
       swap(index, index-1);
-
+      
       //print new list
-      print();
+     // print();
       
       index--;
        
     }
     
+    print();
+  }
+
+
+  public static void print(int colorIndex, int maxBeingSorted) {
+    for (int i=0; i<array.length; i++) { // print unsorted list
+      
+      if (i == colorIndex || i == colorIndex+1) { 
+        System.out.printf(RED); 
+      }
+      else if (i >= maxBeingSorted+1) {
+        System.out.printf(BLUE);
+      }
+      
+      System.out.printf("%d ", array[i]);
+      System.out.printf(RESET); 
+      
+    }
   }
     
 }
